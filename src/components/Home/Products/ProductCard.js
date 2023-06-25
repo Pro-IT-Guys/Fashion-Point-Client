c9ce650070e9c8a31dc6384388a8fa8f5a9f7a6e
Md Imran Hossen <freelancerimran81@gmail.com>
2023-06-25 16:57:59 +0600

    update

--- src/components/Home/Products/ProductCard.js
+++ src/components/Home/Products/ProductCard.js
@@ -6,6 +6,7 @@ import styled from "@emotion/styled";
 import PropTypes from "prop-types";
 import Image from "next/image";
 import img1 from "../../../assets/product/Borka-2-Part-07-fc-01.jpg";
+import { useRouter } from "next/router";
 
 const ProductImgStyle = styled("img")({
   top: 0,
@@ -22,7 +23,8 @@ ProductCard.propTypes = {
 };
 
 export default function ProductCard({ product }) {
-  const { name, price, frontImage, backImage  } = product || {};
+  const { name, price, frontImage, backImage, path } = product || {};
+  const router = useRouter();
 
   console.log(frontImage);
   return (
@@ -30,7 +32,7 @@ export default function ProductCard({ product }) {
       <div className="bg-white ">
         <div className="h-64 w-full overflow-hidden">
           <Image
-            src={`/images/product/${frontImage}`}
+            src={`/images/product/${frontImage} `}
             alt="Picture of the borka"
             height={700}
             width={500}
@@ -38,9 +40,15 @@ export default function ProductCard({ product }) {
           />
         </div>
         <div className="pl-2 bg-white py-2">
-          <h1 className="font-semibold mt-2 text-sm"> {name}</h1>
+          <h1
+            onClick={() => router.push(`/product/${path}`)}
+            className="font-semibold mt-2 text-sm"
+          >
+            {" "}
+            {name}
+          </h1>
           <p className="text-error  mt-2 mb-0">৳ 548</p>
-          <strike className='text-[#7a7a7a] text-xs'>৳ 1,150</strike>
+          <strike className="text-[#7a7a7a] text-xs">৳ 1,150</strike>
         </div>
       </div>
     </div>
