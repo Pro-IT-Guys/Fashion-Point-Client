9355307e7cb16bc0f45f03cff6ed7756dc937c5d
AyonJD <ayonjodder177@gmail.com>
2023-07-02 21:53:03 +0600

    [Done] top nav solved

--- src/layouts/main/TopNavbar.js
+++ src/layouts/main/TopNavbar.js
@@ -1,50 +1,25 @@
-
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { FaFacebookF } from 'react-icons/fa'
 import { IoLogoYoutube } from 'react-icons/io'
 import { BsTwitter } from 'react-icons/bs'
 import { MdEmail } from 'react-icons/md'
 import { BsTelephoneOutboundFill } from 'react-icons/bs'
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ClickAwayListener from '@mui/material/ClickAwayListener';
-import Grow from '@mui/material/Grow';
-import Paper from '@mui/material/Paper';
-import Popper from '@mui/material/Popper';
-import MenuItem from '@mui/material/MenuItem';
-import MenuList from '@mui/material/MenuList';
-import { Container } from '@mui/material'
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
+import ClickAwayListener from '@mui/material/ClickAwayListener'
+import Grow from '@mui/material/Grow'
+import Paper from '@mui/material/Paper'
+import Popper from '@mui/material/Popper'
+import MenuItem from '@mui/material/MenuItem'
+import MenuList from '@mui/material/MenuList'
+import { Container, Menu, Select } from '@mui/material'
 
+const options = ['$ USD', 'د.إ AED']
 
-const options = ['$ USD', "د.إ AED", ];
+export default function TopNavbar() {
+  const [currency, setCurrency] = useState('AED')
 
-
-export default function  TopNavbar () {
-    const [open, setOpen] = React.useState(false);
-    const anchorRef = React.useRef(null);
-    const [selectedIndex, setSelectedIndex] = React.useState(1);
-  
-    const handleClick = () => {
-      console.info(`You clicked ${options[selectedIndex]}`);
-    };
-  
-    const handleMenuItemClick = (event, index) => {
-      setSelectedIndex(index);
-      setOpen(false);
-    };
-  
-    const handleToggle = () => {
-      setOpen((prevOpen) => !prevOpen);
-    };
-  
-    const handleClose = (event) => {
-      if (anchorRef.current && anchorRef.current.contains(event.target)) {
-        return;
-      }
-  
-      setOpen(false);
-    };
   return (
     <div>
       <div className="bg-primary py-1 md:block hidden">
@@ -80,68 +55,15 @@ export default function  TopNavbar () {
                 01868-032281
               </div>
               <div className=" z-50">
-                <ButtonGroup
-                className='text-xs'
-                  variant="contained"
-                  ref={anchorRef}
-                  aria-label="split button"
+                <Select
+                  value={currency}
+                  onChange={e => setCurrency(e.target.value)}
+                  label="Currency"
+                  sx={{ fontSize: '0.75rem' }}
                 >
-                  <Button className='text-xs' onClick={handleClick}>
-                    {options[selectedIndex]}
-                  </Button>
-                  <Button
-                  className='text-xs'
-                    size="small"
-                    aria-controls={open ? 'split-button-menu' : undefined}
-                    aria-expanded={open ? 'true' : undefined}
-                    aria-label="select merge strategy"
-                    aria-haspopup="menu"
-                    onClick={handleToggle}
-                  >
-                    <ArrowDropDownIcon />
-                  </Button>
-                </ButtonGroup>
-                <Popper
-                  sx={{
-                    zIndex: 1,
-                  }}
-                  open={open}
-                  anchorEl={anchorRef.current}
-                  role={undefined}
-                  transition
-                  disablePortal
-                >
-                  {({ TransitionProps, placement }) => (
-                    <Grow
-                      {...TransitionProps}
-                      style={{
-                        transformOrigin:
-                          placement === 'bottom'
-                            ? 'center top'
-                            : 'center bottom',
-                      }}
-                    >
-                      <Paper className='text-xs'>
-                        <ClickAwayListener onClickAway={handleClose}>
-                          <MenuList className='text-xs' id="split-button-menu" autoFocusItem>
-                            {options.map((option, index) => (
-                              <MenuItem
-                                key={option}
-                                disabled={index === 2}
-                                selected={index === selectedIndex}
-                                onClick={event =>
-                                  handleMenuItemClick(event, index)
-                                }
-                              >
-                                {option}
-                              </MenuItem>
-                            ))}
-                          </MenuList>
-                        </ClickAwayListener>
-                      </Paper>
-                    </Grow>
-                  )}
-                </Popper>
+                  <MenuItem value="USD">$ USD</MenuItem>
+                  <MenuItem value="AED"> د.إ AED</MenuItem>
+                </Select>
               </div>
             </div>
           </div>
