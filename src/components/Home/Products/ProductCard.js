ef9b41e3552b13658e93436d5ee0458c6565788f
AyonJD <ayonjodder177@gmail.com>
2023-07-07 01:59:03 +0600

    Image hover effect

--- src/components/Home/Products/ProductCard.js
+++ src/components/Home/Products/ProductCard.js
@@ -1,6 +1,6 @@
 import { Label } from '@mui/icons-material'
 import { Box, Card, Stack, Typography } from '@mui/material'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Link from 'next/link'
 import styled from '@emotion/styled'
 import PropTypes from 'prop-types'
@@ -28,19 +28,48 @@ export default function ProductCard({ product }) {
   const { fromCurrency, toCurrency } = useContext(ContextData)
   const { name, sellingPrice, frontImage, backImage, path } = product || {}
   const router = useRouter()
+  const [hovering, setHovering] = useState(false)
+
+  const handleMouseEnter = () => {
+    setHovering(true)
+  }
+
+  const handleMouseLeave = () => {
+    setHovering(false)
+  }
 
   return (
-    <div className="shadow hover:shadow-md rounded overflow-hidden  h-full">
+    <div className="shadow hover:shadow-md rounded overflow-hidden h-full">
       <div className="bg-white h-full">
-        <div className="h-80 w-full overflow-hidden">
-          <Image
-            // src={`http://localhost:8000/images/product/${frontImage} `}
-            src={frontImage}
-            alt="Picture of the borka"
-            height={1000}
-            width={600}
-            className="object-cover max-h-full w-full"
-          />
+        <div
+          className="h-80 w-full relative overflow-hidden"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div
+            className={`absolute inset-0 transition-opacity duration-300 ${
+              hovering ? 'opacity-0' : 'opacity-100'
+            }`}
+          >
+            <Image
+              src={frontImage}
+              alt="Front Image"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div
+            className={`absolute inset-0 transition-opacity duration-300 ${
+              hovering ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
+            <Image
+              src={backImage}
+              alt="Back Image"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
         <div className="px-2 bg-white py-2">
           <div className="h-14">
@@ -48,14 +77,12 @@ export default function ProductCard({ product }) {
               onClick={() => router.push(`/products/${path}`)}
               className="font-semibold mt-2 h-full text-xs cursor-pointer hover:text-[#4d50ff]"
             >
-              {' '}
               {name.slice(0, 80) + (name.length > 80 ? '...' : '')}
             </h1>
           </div>
-          <p className="text-error  mt-2 mb-0">
+          <p className="text-error mt-2 mb-0">
             {convertCurrency(fromCurrency, toCurrency, sellingPrice)}
           </p>
-          {/* <strike className="text-[#7a7a7a] text-xs">৳ {sellingPrice}</strike> */}
         </div>
       </div>
     </div>
