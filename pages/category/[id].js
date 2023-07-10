4e8cfee5e8e65ea728ad74287883b7c44dfc24e7
Md Imran Hossen <freelancerimran81@gmail.com>
2023-07-10 13:59:31 +0600

    filter category update

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -3,21 +3,27 @@ import { useRouter } from 'next/router'
 import React from 'react'
 import Products from 'src/components/Home/Products/Products'
 import MainLayout from 'src/layouts/main'
-import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
+import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined'
 
 export default function CategoryFilter() {
   const router = useRouter()
   const params = router.query.id
 
-//   console.log(router.pathname);
+  //   console.log(router.pathname);
   return (
     <MainLayout>
       <div className="pt-36 bg-[#f7f7ff9c]">
         <Container maxWidth="lg">
           <div className="flex gap-2 text-sm mt-3 mb-5 text-[#636262]">
-            <p onClick={()=> router.push('/')} className='cursor-pointer hover:underline duration-200' >Home </p> <ChevronRightOutlinedIcon/>
-            <p>Category </p> <ChevronRightOutlinedIcon/>
-            <p className='font-semibold'>{params}</p>
+            <p
+              onClick={() => router.push('/')}
+              className="cursor-pointer hover:underline duration-200"
+            >
+              Home{' '}
+            </p>{' '}
+            <ChevronRightOutlinedIcon />
+            <p>Category </p> <ChevronRightOutlinedIcon />
+            <p className="font-semibold">{params}</p>
           </div>
         </Container>
         <Products />
--- src/components/Home/Products/Products.js
+++ src/components/Home/Products/Products.js
@@ -185,10 +185,10 @@ const Products = () => {
                 )} */}
                 </div>
               </div>
-              {(category.length ||
-                type.length ||
-                style.length ||
-                fabric.length ||
+              {(category?.length ||
+                type?.length ||
+                style?.length ||
+                fabric?.length ||
                 value[0] !== 0 ||
                 value[1] !== 1000) &&
                 !router.pathname.includes('category') && (
@@ -200,7 +200,7 @@ const Products = () => {
                   </div>
                 )}
             </div>
-            <div className="  w-[80%] hidden">
+            <div className="w-[80%] hidden">
               <div className="flex justify-between items-center">
                 <div className="input-group relative flex  items-stretch w-[80%]">
                   <input
@@ -284,8 +284,8 @@ const Products = () => {
                     </div>
                   </div>
                 </div>
-                {router.pathname.includes('category') && (
-                  <div className="bg-white shadow rounded">
+                {/* {router.pathname.includes('category') && (
+                  <div className="bg-white shadow rounded hidden">
                     <div className="  py-2 px-3 border-b">
                       <h1 className="font-semibold "> Filter by Category</h1>
                     </div>
@@ -294,7 +294,7 @@ const Products = () => {
                         value={category}
                         onChange={e => handleSelectFilterOption(e, setCategory)}
                       >
-                        {CATEGORY_OPTION.map(item =>
+                        {CATEGORY_OPTION?.map(item =>
                           item?.classify?.map(item => (
                             <FormControlLabel
                               onClick={() => setCategory(item)}
@@ -308,8 +308,70 @@ const Products = () => {
                       </RadioGroup>
                     </div>
                   </div>
-                )}
-            
+                )} */}
+                <div className="bg-white shadow rounded">
+                  <div className="  py-2 px-3 border-b">
+                    <h1 className="font-semibold "> Filter by Categories</h1>
+                  </div>
+                  <div className=" py-3 pl-4 pr-3">
+                    {/* <RadioGroup
+                      value={fabric}
+                      onChange={e => handleSelectFilterOption(e, setFabric)}
+                    >
+                      {FABRIC_OPTION.map(item =>
+                        item?.classify?.map(item => (
+                          <FormControlLabel
+                            key={item}
+                            value={item}
+                            control={<Radio />}
+                            label={item}
+                          />
+                        ))
+                      )}
+                    </RadioGroup> */}
+
+                    <FormControl fullWidth>
+                      <div>
+                        <Autocomplete
+                          size="small"
+                          className="w-full"
+                          multiple
+                          freeSolo
+                          value={category}
+                          onChange={(event, newValue) => {
+                            setCategory(newValue)
+                          }}
+                          options={CATEGORY_OPTION_ARRAY}
+                          getOptionLabel={option => option}
+                          renderTags={() => null}
+                          renderInput={params => (
+                            <TextField label="Category" {...params} />
+                          )}
+                        ></Autocomplete>
+
+                        <div style={{ marginTop: '8px' }}>
+                          {category?.map((option, index) => (
+                            <Chip
+                              key={option}
+                              size="small"
+                              label={option}
+                              onDelete={() => {
+                                setCategory(prevValue =>
+                                  prevValue?.filter(val => val !== option)
+                                )
+                              }}
+                              deleteIcon={<CloseIcon />}
+                              style={{
+                                marginRight: '8px',
+                                marginBottom: '8px',
+                              }}
+                            />
+                          ))}
+                        </div>
+                      </div>
+                    </FormControl>
+                  </div>
+                </div>
                 <div className="bg-white shadow rounded">
                   <div className="  py-2 px-3 border-b">
                     <h1 className="font-semibold "> Filter by Fabrics</h1>
